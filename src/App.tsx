cadeeb16ec279311651cff4fa1ae3b8321d1e9da
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-02-14 18:09:46 -0300

    add react-error-bounary

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import { useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 const Counter = (props: any) => {
   return <h1>The total counter is {props.counter.toFixed(2)}</h1>;
 };
+
+const ErrorHandler = () => {
+  return (
+    <div>
+      <h2>Ups, sorry</h2>
+      <h3>Something went wrong, but we are working to fix it!</h3>
+    </div>
+  );
+};
+
+function notifyError() {
+  // call our API
+}
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,24 +24,27 @@ function App() {
   return (
     <div
       style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
         width: "100vw",
-        height: "100vh",
+        height: "100%",
       }}
     >
-      <div
+      <h1
         style={{
-          display: "flex",
-          flexDirection: 'column',
-          alignItems: "center",
-          justifyContent: "center",
+          textAlign: "center",
+        }}
+      >
+        Aplicação
+      </h1>
+
+      <ErrorBoundary
+        FallbackComponent={ErrorHandler}
+        onError={(arg1, arg2) => {
+          console.log("arg1", arg1, "arg2", arg2);
         }}
       >
         <Counter /* counter={count} */ />
         <button onClick={() => setCount((old) => old + 1)}>Counter</button>
-      </div>
+      </ErrorBoundary>
     </div>
   );
 }
